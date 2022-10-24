05f26441f707b64ac6409922b99c64231d89951b
Thomas Bourdin <thomas@bourdin.name>
2022-10-24 10:29:24 +0200

    initial commmit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace thomasBourdin\Bundle\KeyValueFormBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('partitech_key_value_form');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('partitech_key_value_form');
+        }
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
---
+++ Form/Type/KeyValueType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace thomasBourdin\Bundle\KeyValueFormBundle\Form\Type;
+
+use thomasBourdin\Bundle\KeyValueFormBundle\Form\DataTransformer\HashToKeyValueArrayTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class KeyValueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new HashToKeyValueArrayTransformer($options['use_container_object']));
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $e) {
+            $input = $e->getData();
+
+            if (null === $input) {
+                return;
+            }
+
+            $output = array();
+
+            foreach ($input as $key => $value) {
+                $output[] = array(
+                    'key' => $key,
+                    'value' => $value
+                );
+            }
+
+            $e->setData($output);
+        }, 1);
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $this->configureOptions($resolver);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        // check if Form component version 2.8+ is used
+        $isSf28 = method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix');
+
+        $resolver->setDefaults(array(
+            $isSf28 ? 'entry_type' : 'type' => $isSf28 ? __NAMESPACE__.'\KeyValueRowType' : 'partitech_key_value_row',
+            'allow_add' => true,
+            'allow_delete' => true,
+            'key_type' => $isSf28 ? 'Symfony\Component\Form\Extension\Core\Type\TextType' : 'text',
+            'key_options' => array(),
+            'value_options' => array(),
+            'allowed_keys' => null,
+            'use_container_object' => false,
+            $isSf28 ? 'entry_options' : 'options' => function(Options $options) {
+                return array(
+                    'key_type' => $options['key_type'],
+                    'value_type' => $options['value_type'],
+                    'key_options' => $options['key_options'],
+                    'value_options' => $options['value_options'],
+                    'allowed_keys' => $options['allowed_keys']
+                );
+            }
+        ));
+
+        $resolver->setRequired(array('value_type'));
+
+        if (method_exists($resolver, 'setDefined')) {
+            // Symfony 2.6+ API
+            $resolver->setAllowedTypes('allowed_keys', array('null', 'array'));
+        } else {
+            // Symfony <2.6 API
+            $resolver->setAllowedTypes(array('allowed_keys' => array('null', 'array')));
+        }
+    }
+
+    public function getParent()
+    {
+        return method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix') ? 'Symfony\Component\Form\Extension\Core\Type\CollectionType' : 'collection';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'partitech_key_value';
+    }
+}
